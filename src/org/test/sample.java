94fedeabbd841c9ac4d320c1ffa06aab96f1cfae
Pradeep-2711 <vttppradeep@gmail.com>
2023-07-15 11:42:47 +0530

    finalreport

---
+++ src/org/test/sample.java
@@ -0,0 +1,16 @@
+package org.test;
+
+public class sample {
+	private void username() {
+		System.out.println("username is pradeep");
+	}
+	
+	private void userphone() {
+		System.out.println("user phone Number is 1234567890 ");
+	}
+public static void main(String[] args) {
+	sample s = new sample();
+	s.username();
+	s.userphone();
+}
+}
