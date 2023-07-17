f464b9e9891b711bac4e9ba67312831c77aa673b
Pradeep-2711 <vttppradeep@gmail.com>
2023-07-17 13:14:33 +0530

    new code by branch1

--- src/org/test/sample.java
+++ src/org/test/sample.java
@@ -8,6 +8,9 @@ public class sample {
 	private void userphone() {
 		System.out.println("user phone Number is 1234567890 ");
 	}
+	private void userid() {
+System.out.println("user id is 123");
+	}
 public static void main(String[] args) {
 	sample s = new sample();
 	s.username();
