df1b4539d7555b68e1974f872a58d995acd4ab12
Pradeep-2711 <vttppradeep@gmail.com>
2023-07-17 10:58:55 +0530

    sample1

---
+++ src/org/test/sample1.java
@@ -0,0 +1,16 @@
+package org.test;
+
+public class sample1 {
+	private void studentname() {
+		System.out.println("username is pradeep");
+	}
+	
+	private void studentphone() {
+		System.out.println("user phone Number is 1234567890 ");
+	}
+public static void main(String[] args) {
+	sample1 s = new sample1();
+	s.studentname();
+	s.studentphone();
+}
+}
